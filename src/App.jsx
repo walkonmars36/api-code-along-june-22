27596d1c92b6bd4a3cfeae437d21baa5e1f1a091
Mark Lawson <mark@walkonmars.co.uk>
2022-06-15 09:02:53 +0100

    finished code along

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,63 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.scss";
-import Button from "./components/Button/Button";
 import CardContainer from "./components/CardContainer/CardContainer";
+import RadioButtons from "./components/RadioButtons/RadioButtons";
+import RangeInput from "./components/RangeInput/RangeInput";
 
 const App = () => {
   const [users, setUsers] = useState([]);
+  const [numberOfUsers, setNumberOfUsers] = useState(7);
+  const [userGender, setUserGender] = useState("all");
 
-  const getUsers = async () => {
-    const url = "https://randomuser.me/api?results=5";
+  const getUsers = async (userNumber, gender) => {
+    let url = "https://randomuser.me/api";
+    if (userNumber) {
+      url += `?results=${userNumber}&gender=${gender}`;
+    }
     const res = await fetch(url);
     const data = await res.json();
     setUsers(data.results);
   };
 
+  // [] APP IS LOADED -> MOUNTING
+  // [DEPENDENCY] -> WHEN YOU WANT THE FUNCTION TO RUN
+
+  useEffect(() => {
+    getUsers(numberOfUsers, userGender);
+  }, [numberOfUsers, userGender]);
+
+  const handleRangeChange = (e) => {
+    console.log(e);
+    setNumberOfUsers(e.target.value);
+  };
+
+  const handleGender = (e) => {
+    setUserGender(e.target.value);
+  };
+
+  ////////////////////////////////////////
+
   return (
     <div className="app">
       <h1>Random User Generator</h1>
-      <Button onClick={getUsers} label="Get Random Users" />
+      <RangeInput
+        label={"Number of users : " + numberOfUsers}
+        id="user-range"
+        value={numberOfUsers}
+        onChange={handleRangeChange}
+      />
+
+      <RadioButtons
+        onChange={handleGender}
+        selected={userGender}
+        options={["all", "male", "female"]}
+        // label={label}
+      />
       <CardContainer cards={users} />
     </div>
   );
 };
 
 export default App;
+
+// onChange, selected, options, label
